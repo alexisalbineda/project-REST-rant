9ea7ae79e8ebf62343ff7a05941b3e95a2992b39
melanie albineda <melaniealbineda@melanies-mbp.lan>
2024-01-24 02:22:29 -0500

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,13 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/funny-sign.jpg" alt="Funny Error Sign"/>
+                    <div>
+                    Photo by <a href="https://unsplash.com/@austinchan?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Austin Chan</a> on <a href="https://unsplash.com/photos/this-is-the-sign-youve-been-looking-for-neon-signage-ukzHlkoz1IE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+                    </div>
+                </div>
             </main>
         </Def>
     )
