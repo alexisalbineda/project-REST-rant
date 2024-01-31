3f031791bc6e920ce28d94b3f896877926281301
melanie albineda <melaniealbineda@melanies-mbp.lan>
2024-01-30 21:10:20 -0500

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -41,7 +41,19 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-
-
+// DELETE 
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
   
 module.exports = router
